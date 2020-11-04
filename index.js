323625049dc3166bc883c6729e7cb1f62474c81d
Jonster5 <49014692+Jonster5@users.noreply.github.com>
2020-11-03 22:08:09 -0500

    username system added

--- index.js
+++ index.js
@@ -1,73 +1,79 @@
 const WebSocket = require('ws');
 const port = process.env.PORT || 8000;
 
-
-const wss = new WebSocket.Server({ port });
-
-let game = {}
+let game = {};
 
 let playerdata = {
-    players: []
-}
+    players: [],
+};
 
 let actions = {
-    return_obj: { frontend: "", frontend_params: [], to: "" },
+    return_obj: { frontend: '', frontend_params: [], to: '' },
     register: (ws, req, username) => {
         let fail = false;
-        playerdata.players.forEach(player => {
-            if (player.username === username) {
-                actions.return_obj = { frontend: "register_fail", frontend_params: [username], to: "single" };
-                fail = true;
-            }
-        });
+        for (player of playerdata.players) {
+            if (player.username !== username) continue;
+            actions.return_obj = { frontend: 'register_fail', frontend_params: [username], to: 'single' };
+            fail = true;
+        }
         if (!fail) {
             playerdata[`client_${username}`] = {
                 username: username,
                 client: ws,
-                ip: req.socket.remoteAddress || req.headers['x-forwarded-for'].split(/\s*,\s*/)[0],
-                userdata: {}
+                ip: req.socket.remoteAddress ||
+                    req.headers['x-forwarded-for'].split(/\s*,\s*/)[0],
+                userdata: {},
             };
 
             playerdata.players.push(playerdata[`client_${username}`]);
 
-            actions.return_obj = { frontend: "register_success", frontend_params: [username], to: "single" }
+            actions.return_obj = {
+                frontend: 'register_success',
+                frontend_params: [username],
+                to: 'single',
+            };
         }
     },
-    join_faction: (ws, req, faction) => {
-
-
-    },
-
+    join_faction: (ws, req, faction) => {},
+    test: (ws, req) => {
+        console.log(playerdata.players);
+    }
 };
 
-wss.on('connection', (ws, req) => {
-    ws.on('message', message => {
+new WebSocket.Server({ port }).on('connection', (ws, req) => {
+    ws.on('message', (message) => {
         let data = JSON.parse(message);
 
         actions[data.backend](ws, req, ...data.backend_params);
 
         if (data.frontend) {
-            if (actions.return_obj.to === "all") {
-                wss.clients.forEach(client => {
+            if (actions.return_obj.to === 'all') {
+                new WebSocket.Server({ port }).clients.forEach((client) => {
                     if (client.readyState === WebSocket.OPEN) {
                         client.send(JSON.stringify(actions.return_obj));
-                        actions.return_obj = { frontend: "", frontend_params: [], to: "" };
+                        actions.return_obj = { frontend: '', frontend_params: [], to: '' };
                     }
                 });
-            } else if (actions.return_obj.to === "broadcast") {
-                wss.clients.forEach(client => {
+            } else if (actions.return_obj.to === 'broadcast') {
+                new WebSocket.Server({ port }).clients.forEach((client) => {
                     if (client !== ws && client.readyState === WebSocket.OPEN) {
                         client.send(JSON.stringify(actions.return_obj));
-                        actions.return_obj = { frontend: "", frontend_params: [], to: "" };
+                        actions.return_obj = { frontend: '', frontend_params: [], to: '' };
                     }
                 });
-            } else if (actions.return_obj.to === "single") {
+            } else if (actions.return_obj.to === 'single') {
                 if (ws.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify(actions.return_obj));
-                    actions.return_obj = { frontend: "", frontend_params: [], to: "" };
+                    ws.send(JSON.stringify(actions.return_obj));
+                    actions.return_obj = { frontend: '', frontend_params: [], to: '' };
                 }
             }
         }
+    });
 
+    ws.on('close', () => {
+        for (player of playerdata.players) {
+            if (player.client !== ws) continue;
+            playerdata.players.splice(playerdata.players.indexOf(player), 1);
+        }
     });
 });
